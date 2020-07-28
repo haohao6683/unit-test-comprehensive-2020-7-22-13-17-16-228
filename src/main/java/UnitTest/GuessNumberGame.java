cade228434b1c7b9a1fdc81f64d0fec07faacef8
haohao6683 <852721803@qq.com>
2020-07-28 21:27:15 +0800

    [Guess number game][Draymond] Refactor: Remove some useless code.

--- src/main/java/UnitTest/GuessNumberGame.java
+++ src/main/java/UnitTest/GuessNumberGame.java
@@ -10,21 +10,21 @@ public class GuessNumberGame implements Game{
         this.answer = answer;
     }
 
+    //todo guessNumber
     public String guess(String inputString){
-        if(inputString.equals(answer)){
-            return "4A0B";
-        }
         char[] inputList = inputString.toCharArray();
         char[] answerList = answer.toCharArray();
 
         int aNum = generateA(inputList,answerList);
         int bNum = generateB(inputList,answerList);
-
+        //todo String.format
         return aNum + "A" + bNum + "B";
     }
-
+    //todo method name
     private int generateB(char[] inputList, char[] answerList) {
         int bNum = 0;
+        //todo index naming
+        //todo stream
         for(int i=0; i < inputList.length; i++)
             for(int j=0; j < answerList.length; j++){
                 if(i != j && inputList[i] == answerList[j]){
@@ -56,6 +56,7 @@ public class GuessNumberGame implements Game{
             hashSet.add(String.valueOf(input.charAt(i)));
         }
         return hashSet.size() == input.length();
+        //todo useless
 //        for (int i = 0; i < 4; i++) {
 //            if (input.lastIndexOf(input.charAt(i)) > i) {//查重
 //                return false;
@@ -66,10 +67,12 @@ public class GuessNumberGame implements Game{
     public int getInput(){
         Scanner s = new Scanner(System.in);
         int inputCount = 0;
+        //todo
         while (inputCount <= 6) {
             inputCount++;
             String input = s.next();
             if (!validate(input)) {
+                //todo magic number
                 System.out.println("Wrong Input，Input again");
             }
             else{
